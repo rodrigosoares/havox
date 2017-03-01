1773095c792a584e8b9a173be277318e2008b503
Rodrigo Soares <silvars1@gmail.com>
2017-03-01 13:35:36 -0300

    REFS #7: Added some description to the Rule class code.

--- lib/havox/rule.rb
+++ lib/havox/rule.rb
@@ -21,18 +21,18 @@ module Havox
 
     def parsed_matches(raw_rule)
       ok_matches = {}
-      raw_matches = raw_rule.split('->').first.split('and')
-      raw_matches = raw_matches.map { |str| str.tr('()*', '').strip }
-      raw_matches = raw_matches.reject(&:empty?)
+      raw_matches = raw_rule.split('->').first.split('and')                     # Parses each match field in the 1st part.
+      raw_matches = raw_matches.map { |str| str.tr('()*', '').strip }           # Removes unwanted characters.
+      raw_matches = raw_matches.reject(&:empty?)                                # Rejects resulting empty match fields.
       raw_matches.each do |raw_match|
-        stmt = raw_match.split(/\s?=\s?/)
-        ok_matches[DIC[stmt.first].to_sym] = stmt.last
+        stmt = raw_match.split(/\s?=\s?/)                                       # Splits the statement by '='
+        ok_matches[DIC[stmt.first].to_sym] = stmt.last                          # Adds a treated entry based on the dictionary.
       end
       ok_matches
     end
 
     def parsed_action(raw_rule)
-      raw_rule.split('->').last.strip
+      raw_rule.split('->').last.strip                                           # Parses the action in the 2nd part.
     end
   end
 end
