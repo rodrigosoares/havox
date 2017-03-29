093351a5a6ef28a3159b63cba465ac23a7b68c3f
Rodrigo Soares <silvars1@gmail.com>
2017-03-29 00:17:02 -0300

    REFS #20: Added datapath disconnection handler.

--- lib/trema/controllers/main_controller.rb
+++ lib/trema/controllers/main_controller.rb
@@ -2,17 +2,36 @@ require 'havox'
 require 'colorize'
 
 class MainController < Trema::Controller
+  timer_event :datapath_statuses, interval: 10.sec
+
   def start(_argv)
     logger.info "Generating rules based on the policies defined in #{ENV['MERLIN_POLICY'].bold}" \
                 " over the topology #{ENV['MERLIN_TOPOLOGY'].bold}..."
     @rules = Havox::Policies.compile!(ENV['MERLIN_TOPOLOGY'], ENV['MERLIN_POLICY'])
     logger.info "Generated #{@rules.size} Merlin rules."
     @datapaths = []
+    @datapaths_off = []
   end
 
   def switch_ready(dp_id)
     @datapaths << dp_id
+    @datapaths_off -= [dp_id]
     dp_name = "s#{dp_id}"
     logger.info "Datapath #{dp_name.bold} is #{'ONLINE'.bold.green}."
+  end
+
+  def switch_disconnected(dp_id)
+    @datapaths -= [dp_id]
+    @datapaths_off << dp_id
+    dp_name = "s#{dp_id}"
+    logger.info "Datapath #{dp_name.bold} is #{'OFFLINE'.bold.red}"
+  end
+
+  private
+
+  def datapath_statuses
+    datapaths_on  = @datapaths.map { |id| "s#{id}" }.join(' ').bold.green
+    datapaths_off = @datapaths_off.map { |id| "s#{id}" }.join(' ').bold.red
+    logger.info "Datapath statuses: [#{datapaths_on}] [#{datapaths_off}]"
   end
 end
