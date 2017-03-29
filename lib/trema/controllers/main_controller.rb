1ed095887acca3e69dc26aff496cf29a829b86ea
Rodrigo Soares <silvars1@gmail.com>
2017-03-28 21:14:16 -0300

    REFS #20: Called Havox from inside Trema controller successfully.

--- lib/trema/controllers/main_controller.rb
+++ lib/trema/controllers/main_controller.rb
@@ -1,11 +1,17 @@
+require 'havox'
+
 class MainController < Trema::Controller
   def start(_argv)
     @datapaths = []
-    logger.info 'Havox service is active.'
+    logger.info 'Havox service is ACTIVE.'
+    logger.info "Using Merlin topology from #{ENV['MERLIN_TOPOLOGY']}."
+    logger.info "Using Merlin policy from #{ENV['MERLIN_POLICY']}."
+    @rules = Havox::Policies.compile!(ENV['MERLIN_TOPOLOGY'], ENV['MERLIN_POLICY'])
+    logger.info "The number of generated Merlin rules is #{@rules.size}"
   end
 
   def switch_ready(dp_id)
     @datapaths << dp_id
-    logger.info "Datapath ID #{dp_id} is online."
+    logger.info "Datapath s#{dp_id} is ONLINE."
   end
 end
