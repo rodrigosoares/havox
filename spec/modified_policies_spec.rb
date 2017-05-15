d80452fed2a873eeb68f3b107ed5105bf3433030
Rodrigo Soares <silvars1@gmail.com>
2017-05-14 21:46:10 -0300

    REFS #31: Improved Havox::ModifiedPolicy testing code.

--- spec/modified_policies_spec.rb
+++ spec/modified_policies_spec.rb
@@ -14,21 +14,19 @@ describe Havox::ModifiedPolicy do
       allow(File).to receive(:read).with('/path/to/policy.mln').and_return(policy_file.string)
       allow(Tempfile).to receive(:open).with(['policy', '.mln']).and_yield(mod_policy_file)
       allow_any_instance_of(StringIO).to receive(:path).and_return('/tmp/policy_mod.mln')
+      @mp = Havox::ModifiedPolicy.new('/path/to/topology.dot', '/path/to/policy.mln')
     end
 
     it 'copies the policy file to a temporary modified file' do
-      mp = Havox::ModifiedPolicy.new('/path/to/topology.dot', '/path/to/policy.mln')
-      expect(mp.path).to eq('/tmp/policy_mod.mln')
+      expect(@mp.path).to eq('/tmp/policy_mod.mln')
       expect(mod_policy_file.string).not_to be_empty
     end
 
     it 'appends a policy for the ICMP protocol' do
-      mp = Havox::ModifiedPolicy.new('/path/to/topology.dot', '/path/to/policy.mln')
       expect(mod_policy_file.string).to include('ethTyp = 2048 and ipProto = 1 -> .* at min(100 Mbps);')
     end
 
     it 'appends a policy for the ARP protocol' do
-      mp = Havox::ModifiedPolicy.new('/path/to/topology.dot', '/path/to/policy.mln')
       expect(mod_policy_file.string).to include('ethTyp = 2054 -> .* at min(100 Mbps);')
     end
   end
