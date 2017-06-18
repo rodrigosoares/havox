cd1156cd187130b2547e00b2e7c8107dd2d6754c
Rodrigo Soares <silvars1@gmail.com>
2017-06-18 01:08:26 -0300

    #38: Converted string numer in RouteFlow actions to integer.

--- spec/routeflow_actions_spec.rb
+++ spec/routeflow_actions_spec.rb
@@ -12,15 +12,15 @@ describe Havox::OpenFlow10::RouteFlow::Actions do
   describe '.treat' do
     it 'translates actions from Merlin format to RouteFlow format' do
       of_actions = subject.treat(mln_actions)
-      expect(of_actions).to include({ action: :rfat_output, arg_a: '1', arg_b: nil })
-      expect(of_actions).to include({ action: :rfat_enqueue, arg_a: '2', arg_b: '2' })
+      expect(of_actions).to include({ action: :rfat_output, arg_a: 1, arg_b: nil })
+      expect(of_actions).to include({ action: :rfat_enqueue, arg_a: 2, arg_b: 2 })
       expect(of_actions).to include({ action: :rfat_strip_vlan, arg_a: nil, arg_b: nil })
-      expect(of_actions).to include({ action: :rfat_set_vlan_id, arg_a: '2', arg_b: nil })
+      expect(of_actions).to include({ action: :rfat_set_vlan_id, arg_a: 2, arg_b: nil })
     end
 
     it 'switches Enqueue for Output if specified' do
       of_actions = subject.treat(mln_actions, { output: true })
-      expect(of_actions).to include({ action: :rfat_output, arg_a: '2', arg_b: nil })
+      expect(of_actions).to include({ action: :rfat_output, arg_a: 2, arg_b: nil })
       expect(of_actions.map { |a| a[:action]}).not_to include(:rfat_enqueue)
     end
 
