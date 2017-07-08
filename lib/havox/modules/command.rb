6a4e582d64df09ed3c88477f572c2bb87ac530be
Rodrigo Soares <silvars1@gmail.com>
2017-07-08 17:24:44 -0300

    #41: Removed unused Quagga config methods and files.

--- lib/havox/modules/command.rb
+++ lib/havox/modules/command.rb
@@ -9,32 +9,6 @@ module Havox
         rf_command(vm_name, "/usr/bin/vtysh -c '#{command}'")
       end
 
-      def toggle_daemon(vm_name, daemon, activate = true)
-        new_mode = activate ? 'yes' : 'no'
-        old_mode = activate ? 'no' : 'yes'
-        rf_command(vm_name, "/bin/sed -i 's/#{daemon.to_s}=#{old_mode}/#{daemon.to_s}=#{new_mode}/' /etc/quagga/daemons")
-      end
-
-      def copy_conf_files(vm_name, daemon)
-        rf_command(vm_name, "/bin/cp /usr/share/doc/quagga/examples/#{daemon}.conf.sample /etc/quagga/#{daemon}.conf")
-      end
-
-      def toggle_service(vm_name, service, action)
-        rf_command(vm_name, "#{service} #{action}")
-      end
-
-      def backup(vm_name, path)
-        rf_command(vm_name, "/bin/cp #{path} #{path}.old")
-      end
-
-      def chown(vm_name, user, group, path)
-        rf_command(vm_name, "/bin/chown #{user}:#{group} #{path}")
-      end
-
-      def chmod(vm_name, permissions, path)
-        rf_command(vm_name, "/bin/chmod #{permissions} #{path}")
-      end
-
       def compile(topology_file, policy_file)
         merlin_command("-topo #{topology_file} #{policy_file} -verbose")
       end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -17,60 +17,6 @@ describe Havox::Command do
     end
   end
 
-  describe '.toggle_daemon' do
-    it 'builds a remote daemon activating command' do
-      returned_cmd = subject.toggle_daemon('foo_vm', :bgpd)
-      expected_cmd = "sudo lxc-attach -n foo_vm -- /bin/sed -i 's/bgpd=no/bgpd=yes/' /etc/quagga/daemons"
-      expect(returned_cmd).to eq(expected_cmd)
-    end
-
-    it 'builds a remote daemon deactivating command' do
-      returned_cmd = subject.toggle_daemon('foo_vm', :bgpd, false)
-      expected_cmd = "sudo lxc-attach -n foo_vm -- /bin/sed -i 's/bgpd=yes/bgpd=no/' /etc/quagga/daemons"
-      expect(returned_cmd).to eq(expected_cmd)
-    end
-  end
-
-  describe '.toggle_service' do
-    it 'builds a remote service toggling command' do
-      returned_cmd = subject.toggle_service('foo_vm', '/path/to/service', :restart)
-      expected_cmd = 'sudo lxc-attach -n foo_vm -- /path/to/service restart'
-      expect(returned_cmd).to eq(expected_cmd)
-    end
-  end
-
-  describe '.copy_conf_files' do
-    it 'builds a daemon configuration file setting command' do
-      returned_cmd = subject.copy_conf_files('foo_vm', :bgpd)
-      expected_cmd = 'sudo lxc-attach -n foo_vm -- /bin/cp /usr/share/doc/quagga/examples/bgpd.conf.sample /etc/quagga/bgpd.conf'
-      expect(returned_cmd).to eq(expected_cmd)
-    end
-  end
-
-  describe '.backup' do
-    it 'builds a backup command' do
-      returned_cmd = subject.backup('foo_vm', '/path/to/file')
-      expected_cmd = 'sudo lxc-attach -n foo_vm -- /bin/cp /path/to/file /path/to/file.old'
-      expect(returned_cmd).to eq(expected_cmd)
-    end
-  end
-
-  describe '.chown' do
-    it 'builds a file owner changing command' do
-      returned_cmd = subject.chown('foo_vm', 'bar', 'bar', '/path/to/file')
-      expected_cmd = 'sudo lxc-attach -n foo_vm -- /bin/chown bar:bar /path/to/file'
-      expect(returned_cmd).to eq(expected_cmd)
-    end
-  end
-
-  describe '.chmod' do
-    it 'builds a file permissions changing command' do
-      returned_cmd = subject.chmod('foo_vm', 776, '/path/to/file')
-      expected_cmd = 'sudo lxc-attach -n foo_vm -- /bin/chmod 776 /path/to/file'
-      expect(returned_cmd).to eq(expected_cmd)
-    end
-  end
-
   describe '.compile' do
     let(:config) { Havox.configuration }
 
