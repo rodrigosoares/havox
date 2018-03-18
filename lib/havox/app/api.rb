8af7457733ac801a8f71e480624b87543c220f8a
Rodrigo Soares <silvars1@gmail.com>
2018-03-18 18:44:02 -0300

    Fix undefined helper method error.

--- lib/havox/app/api.rb
+++ lib/havox/app/api.rb
@@ -1,30 +1,14 @@
-require 'bundler/setup' # TODO: Remove this line after Havox is released to RubyGems.
 require 'sinatra'
-require 'colorize'
-require 'havox'
 require_relative '../../../config'
-require_relative 'helpers/methods'
+require_relative 'helpers/api_helper'
 
 module Havox
   class API < Sinatra::Base
+    helpers Havox::APIHelper
+
     set :bind, '0.0.0.0'
 
     before { content_type :json }
-
-    # TODO: Make this read files from anywhere in the file system.
-    get '/rules/:name' do
-      base_dir = '/home/rod/Projetos/Mestrado/havox/lib/merlin'
-      opts = {
-        merlin_topology: "#{base_dir}/topologies/#{params[:name]}.dot",
-        merlin_policy: "#{base_dir}/policies/#{params[:name]}.mln",
-        force: params[:force].eql?('true'),
-        basic: params[:basic].eql?('true'),
-        expand: params[:expand].eql?('true'),
-        output: params[:output].eql?('true'),
-        syntax: params[:syntax]&.to_sym
-      }
-      Havox::Policy.new(opts).to_json
-    end
 
     post '/rules' do
       dot_filepath = handle_file(params[:dot_file])
---
+++ lib/havox/app/helpers/api_helper.rb
@@ -0,0 +1,45 @@
+module Havox
+  module APIHelper
+    def handle_file(params)
+      filepath = "./#{params[:filename]}"
+      File.open(filepath, 'w') { |f| f.write(params[:tempfile].read) }
+      filepath
+    end
+
+    def run_network(dot_filepath, hvx_filepath, opts = {})
+      mln_filename = "./#{File.basename(hvx_filepath, '.hvx')}.mln"
+      eval File.read(hvx_filepath)
+      mln_blocks = Havox::Network.transcompile(opts)
+      print_blocks(mln_blocks, opts)
+      return nil if mln_blocks.empty?
+      File.open(mln_filename, 'w') { |f| f.write(mln_blocks.join("\n")) }
+      mln_filename
+    end
+
+    def print_blocks(merlin_blocks, opts)
+      print_opts(opts)
+      if merlin_blocks.empty?
+        puts 'No Merlin code was generated'.bold.red
+      else
+        puts 'Merlin code generated:'.bold
+        puts merlin_blocks.join("\n").green
+      end
+    end
+
+    def print_policy(policy, opts)
+      print_opts(opts)
+      puts "Generated #{policy.rules.size} rule(s)".bold
+    end
+
+    def print_opts(opts)
+      puts 'Havox will:'.bold
+      puts "-- generate Merlin code with QoS '#{opts[:qos]}'".blue unless opts[:qos].nil?
+      puts "-- generate Merlin code with preference for exit switches: #{opts[:preferred].join(', ')}".blue unless opts[:preferred].nil?
+      puts "-- generate Merlin code with arbitrary exit switches: #{opts[:arbitrary].join(', ')}".blue unless opts[:arbitrary].nil?
+      puts '-- force attribute redefinition for rules with an attribute defined twice'.blue if opts[:force]
+      puts '-- automatically append policies for ARP and ICMP protocols'.blue if opts[:basic]
+      puts '-- expand generated rules from VLAN-based to their full predicates'.blue if opts[:expand]
+      puts '-- switch all occurrences of Enqueue action to Output action'.blue if opts[:output]
+    end
+  end
+end
