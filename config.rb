8c47e5596f55f374f1c14943dc82e29f687597c6
Rodrigo Soares <silvars1@gmail.com>
2018-03-19 19:33:56 -0300

    Change routing instances to 2x2 settings.

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
 Havox.configure do |config|
   config.merlin_host = '192.168.56.102'
-  config.rf_lxc_names = %w(rfvmA rfvmB rfvmC rfvmD rfvmE rfvmF rfvmG rfvmH rfvmI)
+  config.rf_lxc_names = %w(rfvmA rfvmB rfvmC rfvmD)
 end
